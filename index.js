c08688858443ffaca98941b190d525ea4ba87089
Brian07 <hoanganh0594abc@gmail.com>
2023-02-13 08:40:26 +0700

    init axios and ios

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
-import {gestureHandlerRootHOC } from 'react-native-gesture-handler';
+import { name as appName } from './app.json';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+import initInterceptor from './src/utils/axios';
+
+initInterceptor();
 
 AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
--- src/components/SafeArea/index.tsx
+++ src/components/SafeArea/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView  } from 'react-native';
+import { SafeAreaView } from 'react-native';
 import styled from "styled-components/native";
 
 const SafeAreaStyle = styled.SafeAreaView`
