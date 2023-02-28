7a0cc0e153542da9c8937d6bd015345b0f1f0df2
Brian07 <hoanganh0594abc@gmail.com>
2023-02-28 19:52:42 +0700

    add query

---
+++ src/hook/useCallApi/index.tsx
@@ -0,0 +1,33 @@
+import { AxiosError, AxiosResponse } from 'axios';
+import { isFunction } from 'lodash';
+import { useState } from 'react';
+import { UseCallApiParams } from './index.types';
+
+const useCallApi = <T = any>(props: UseCallApiParams<T>) => {
+  const { error, request, success } = props;
+  const [isLoading, setIsLoading] = useState(false);
+  const send = async (...params: Array<any>) => {
+    try {
+      setIsLoading(true);
+      const response = (await request(...params)) as AxiosResponse;
+      const data = response?.data || response;
+      if (isFunction(success)) {
+        success(data);
+      }
+    } catch (err) {
+      const e = err as unknown as AxiosError;
+      if (isFunction(error)) {
+        error(e?.message || e);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return {
+    send,
+    isLoading,
+  };
+};
+
+export default useCallApi;
