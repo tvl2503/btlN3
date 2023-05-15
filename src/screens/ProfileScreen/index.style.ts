2600438e31c21fddfe5e7df8758c850281f2542b
tvl2503 <volinhh1804@gmail.com>
2023-05-15 21:47:39 +0700

    feature-logout

--- src/screens/ProfileScreen/index.style.ts
+++ src/screens/ProfileScreen/index.style.ts
@@ -1,6 +1,23 @@
 import styled from "styled-components/native";
 import BoxActions from "./ui/BoxActions";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export const BoxActionsComposed = styled(BoxActions)` 
   margin-top: ${props => props.theme.sizes.xl};
 `;
+
+export const LogoutContainer = styled(View)`
+  flex: 1;
+  justifyContent: flex-end;
+  margin: 16px;
+`
+export const LogoutButton = styled(TouchableOpacity)`
+  borderColor: ${props => props.theme.colors.danger};
+  borderWidth: 1px;
+  alignItems: center;
+  padding: 8px 16px;
+  borderRadius: 8px;
+`
+export const TextButton = styled(Text)`
+  color: ${props => props.theme.colors.danger};
+`
--- src/screens/ProfileScreen/index.tsx
+++ src/screens/ProfileScreen/index.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import SafeArea from '../../core/SafeArea';
 import BackgroundContainer from './ui/BackgroundContainer';
-import { BoxActionsComposed } from './index.style';
+import { BoxActionsComposed, LogoutButton, LogoutContainer, TextButton } from './index.style';
+import { useDispatch } from 'react-redux';
+import { useAction } from '../../helper/ActionProvider';
+import { userActions } from '../../store/slices/user';
+
 
 const ProfileScreen = () => {
+  const dispatch = useDispatch();
+
+  const onHandleLogout = () => {
+    dispatch(userActions.logout())
+  }
   return (
     <SafeArea>
       <>
         <BackgroundContainer />
         <BoxActionsComposed />
+        <LogoutContainer>
+
+        <LogoutButton onPress={onHandleLogout} >
+          <TextButton >Đăng xuất</TextButton>
+        </LogoutButton>
+        </LogoutContainer>
       </>
     </SafeArea>
   );
